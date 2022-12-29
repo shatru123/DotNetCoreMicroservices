63ee2299814f76ddfed7ab0e0cbf5b3dd173e71b
Shatru123 <ambhoreshatrughna@gmail.com>
2022-12-29 18:29:16 +0530

    resolved docker issues with discount api

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,9 +1,9 @@
+using Discount.API.Extensions;
 using Discount.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,7 +17,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MigrateDatabase<WebSocketAcceptContext>();
 app.UseAuthorization();
 
 app.MapControllers();
